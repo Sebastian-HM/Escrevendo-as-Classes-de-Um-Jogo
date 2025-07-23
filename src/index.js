8bd8008af8a9a3a62728a8038b548af5c1e52241
Sebastian-HM <sebadastoli@gmail.com>
2025-07-22 23:06:46 -0300

    Acrescentei 2 const e 2

--- src/index.js
+++ src/index.js
@@ -43,4 +43,32 @@ class Heroi {
         this.idade = idade;
         this.tipo = tipo;
     }
+
+    atacar() {
+        let ataque = "";
+
+        switch (this.tipo) {
+            case "mago":
+                ataque = "usou magia";
+                break;
+
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+
+            case "ninja":
+                ataque = "usou shuriken";
+                break;
+
+            default:
+                ataque = "ataque desconhecido";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+    
 }
